c3379396612d500b2e7d2b1073f5f704cde3dfe4
renansantos <renansantos@renansantos-Lenovo-G480>
2018-02-10 11:40:16 -0200

    - erro no calculo do RRF corrigido -> estava dividindo zero por zero - inicio da correção do erro do calculo do tempo de antecipação -> inicialmente corrigido
    - implementação do algoritmo VNS
    - implementação do Simulated Annealing
    - método para realização de experimentos concluído
    - parâmetros setados
    - temperatura inicial calculada com base nas 10 soluções vizinhas dado uma solução inicial
    
    
    
    - NÃO ESTÁ PERCORRENDO TODAS AS SOLICITAÇÕES PARA INSERIR -> CORRIGIR
    - CORRIGIR VIZINHANÇA DE REALOCAÇÃO DE SOLICITAÇÕES -> a solicitação não é retirada da primeira rota e é inserida na segunda, assim o valor da função objetivo vai ser sempre maior, fazendo com que não haja nenhuma solução que seja melhorada com essa vizinhança
    - CORREÇÃO ANTERIOR ESTÁ SENDO FEITA + falta arrumar as rotas
    - implementação do algoritmo VND
    - falta implementar o algoritmo de pós-otimização
    - falta testar e achar onde esta o erro da repetição de solicitações em rotas distintas

---
+++ src/main/java/com/mycompany/hibernateproject/Movie.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hibernateproject;
+
+/**
+ *
+ * @author renansantos
+ */
+public class Movie {
+
+    private int id = 0;
+    private String title = null;
+    private String synopsis = null;
+    private String director = null;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    
+    
+}
